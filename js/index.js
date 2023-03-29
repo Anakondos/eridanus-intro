84babc23bfb2ae8442a0a8958b2cb89435faf207
Anakondos <anakondos@gmail.com>
2023-03-28 22:15:24 -0400

    add flex

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ const thisYear = today.getFullYear();
 // console.log(thisYear);
 const footer = document.querySelector('#footer');
 const copyright = document.createElement('p');
-console.log(copyright);
+// console.log(copyright);
 copyright.innerHTML = 'Anton Kondakov &copy ' + thisYear;
 footer.appendChild(copyright); 
-let skills = ['Agile', 'JavaScript', 'HTML', 'CSS' ];
+let skills = ['Agile', 'JavaScript', 'HTML', 'CSS', 'Business Analytics', 'Flexbox', 'CSS Gride', 'NodeJS', 'sweet sam' ];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 // console.log(skillsList);
@@ -17,3 +17,41 @@ for(let i = 0; i < skills.length; i++) {
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+
+//Create Massage form
+const messageForm = document.getElementsByName('leave_message');
+
+messageForm[0].addEventListener("submit", function answer(evt){
+    evt.preventDefault();
+    
+    const name = evt.target.name.value;
+    const email = evt.target.email.value;
+    const message = evt.target.userMessage.value;
+
+    console.log(name, email, message);
+    
+    const messageSection = document.getElementById("messages");
+    
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href=mailto:${email}>${name}</a><span> wrote: ${message} </span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.setAttribute('type','button');
+    
+        removeButton.addEventListener('click', (event)=>{
+            const entry = event.target.parentNode;
+            // console.log(entry);
+            entry.remove();
+            if(messageList.childElementCount === 0){
+                messageSection.style.display = 'none';
+            }
+
+        });
+        messageSection.style.display = "block";
+    newMessage.appendChild(removeButton); 
+    messageList.appendChild(newMessage);
+   
+    messageForm.reset();
+});
